bc5237757cabaddbf9b42d8482ccafcf696eb3e1
Chris Adkins <c.adkins19967@gmail.com>
2017-10-17 15:28:13 -0400

    working on map

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -6,17 +6,17 @@
         .controller('mainController', function(data) {
             const vm = this;
 
-            function initMap() {
-                var location = {lat: -25.363, lng: 131.044};
-                var map = new google.maps.Map(document.getElementById('map'), {
-                    zoom: 4,
-                    center: location
-                });
-                var marker = new google.maps.Marker({
-                position: location,
-                map: map
-                });
-            }
+            // function initMap() {
+            //     var uluru = {lat: -25.363, lng: 131.044};
+            //     var map = new google.maps.Map(document.getElementById('map'), {
+            //         zoom: 4,
+            //         center: uluru
+            //     });
+            //     var marker = new google.maps.Marker({
+            //     position: uluru,
+            //     map: map
+            //     });
+            // }
             
         })
 })();
